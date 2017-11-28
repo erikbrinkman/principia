98cfbef0ee0110e554dbb80ce721930905b6911c
Erik Brinkman <erik.brinkman@gmail.com>
2017-11-28 11:38:02 -0500

    Add test for index test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,23 @@
 import {} from "jest";
 import * as cp from "child_process";
-import * as backend from "../src/backend";
-import * as curves from "../src/curves";
-import * as rects from "../src/rects";
+import * as diff from "diff";
+import * as fs from "fs";
+import * as check from "./check";
+import * as utils from "../src/utils";
 
 
 describe('principia', () => {
   it('executes the example', done => {
-    cp.exec('node node_modules/headlesspdf/cmd.js -i test/ex.js -c test/ex.css -o test/ex.pdf',
+    cp.exec('node node_modules/headlesspdf/cmd.js -i test/ex.js -c test/ex.css',
       (err, stdout, stderr) => {
         if (err) {
           done.fail(stderr);
         } else {
-          // FIXME Test that result matches what we expect
+          const expected = fs.readFileSync("test/ex.pdf", "utf8");
+          const differ = diff.diffChars(expected, stdout);
+          const total = utils.sum(differ.map(d => d.count));
+          const changed = utils.sum(differ.filter(d => d.added || d.removed).map(d => d.count));
+          check(changed / total < 0.001);
           done();
         }
     });
